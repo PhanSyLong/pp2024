b150949a62f3f7d2d653417111b4c17ac9a770ed
Phan Sy Long <phansylong12mk@gmail.com>
2024-01-16 22:02:36 +0700

    First student commit

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,39 +1,54 @@
 def inputNumberOfStudents():
-    numOfStudent = input("Enter number of students:")
-    return int(numOfStudent)
+    numOfStudents = input("Enter number of students: ")
+    return int(numOfStudents)
 
 def inputOneStudent():
-    id = input("id: ")
+    student_id = input("id: ")
     name = input("name: ")
     dob = input("dob: ")
     student = {
-        'id': id,
+        'id': student_id,
         'name': name,
-        'dob' : dob
+        'dob': dob
     }
     return student
 
 def inputNumberOfCourses():
-    numOfCourses = input("Enter number of courses:")
+    numOfCourses = input("Enter number of courses: ")
     return int(numOfCourses)
 
-def inputOneStudent():
-    id = input("id: ")
+def inputOneCourse():
+    course_id = input("id: ")
     name = input("name: ")
-    courses = {
-        'id': id,
+    course = {
+        'id': course_id,
         'name': name
     }
-    return courses
+    return course
 
-def selectCoursesAndInputsMarks(course, marks, students):
-    print(f"Enter marks for student in courses {course['name']}")
+def selectCoursesAndInputsMarks(courses, students):
     marks = {}
-    marks[course['id']] = []
-    for student in students:
-        mark = float(input(f"Enter marks for student {student['name']}"))
-        studentMark = {
-            'studentid': student['id'],
-            'mark': mark
-        }
-        marks[course['id']] += [studentMark]
+    for course in courses:
+        print(f"Enter marks for students in course {course['name']}")
+        marks[course['id']] = []
+        for student in students:
+            mark = float(input(f"Enter mark for student {student['name']}: "))
+            studentMark = {
+                'student_id': student['id'],
+                'mark': mark
+            }
+            marks[course['id']].append(studentMark)
+    return marks
+
+num_of_students = inputNumberOfStudents()
+students = [inputOneStudent() for _ in range(num_of_students)]
+
+num_of_courses = inputNumberOfCourses()
+courses = [inputOneCourse() for _ in range(num_of_courses)]
+
+marks = selectCoursesAndInputsMarks(courses, students)
+
+print("Collected data:")
+print("Students:", students)
+print("Courses:", courses)
+print("Marks:", marks)
